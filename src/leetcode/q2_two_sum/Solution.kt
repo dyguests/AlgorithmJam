cab9de8ad6f0ead2b312989de02bba0c91f77b3d
fanhl <dyguests@qq.com>
2022-05-22 20:23:10 +0800

    init.

---
+++ src/leetcode/q2_two_sum/Solution.kt
@@ -0,0 +1,16 @@
+package leetcode.q2_two_sum
+
+import java.security.InvalidParameterException
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        for (i in nums.indices) {
+            for (j in (i + 1) until nums.size) {
+                if (nums[i] + nums[j] == target) {
+                    return intArrayOf(i, j)
+                }
+            }
+        }
+        throw InvalidParameterException()
+    }
+}
