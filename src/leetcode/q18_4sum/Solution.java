9add8503ea41ca7b922bfdbd1c250b0e27a0be9c
fanhl <dyguests@qq.com>
2022-06-20 23:06:17 +0800

    getSwingIndex

--- src/leetcode/q18_4sum/Solution.java
+++ src/leetcode/q18_4sum/Solution.java
@@ -20,17 +20,20 @@ class Solution {
         HashSet<Integer> usedIndexes = new HashSet<>();
         int remainder = target;
         for (int k = n; k > 0; k--) {
-            int indexOfNums = binarySearchClosest(nums, remainder / k);
-            //            swingTraversal(nums, indexOfNums, index ->)
+            int closestIndex = binarySearchClosest(nums, remainder / k);
+            //            swing(nums, closestIndex);
+            for (int i = 0; i < nums.length; i++) {
+                int swingIndex = getSwingIndex(closestIndex, nums.length, i);
+            }
         }
         return new ArrayList<>();
     }
 
-    public static int binarySearchClosest(int[] array, int key) {
+    static int binarySearchClosest(int[] array, int key) {
         return binarySearchClosest0(array, 0, array.length, key);
     }
 
-    private static int binarySearchClosest(int[] array, int fromIndex, int toIndex, int key) {
+    static int binarySearchClosest(int[] array, int fromIndex, int toIndex, int key) {
         return binarySearchClosest0(array, fromIndex, toIndex, key);
     }
 
@@ -53,5 +56,22 @@ class Solution {
         }
 
         return mid;
+    }
+
+    /**
+     * 从总长度为length的array中，取得从closestIndex开始的第index个swingIndex
+     * <p>
+     * 这是一个类似fori的索引遍历，依次取closestIndex, start+1, start-1, start+2, start-2, ...
+     * <p>
+     * 例： 当 start=2, length=7, index从0取值到6时，返回值分别为： 2,3,1,4,0,5,6
+     */
+    static int getSwingIndex(int start, int length, int index) {
+        if (index > start * 2) {
+            return index;
+        }
+        if (index > (length - 1 - start) * 2) {
+            return length - 1 - index;
+        }
+        return start + (((index & 1) == 1) ? 1 : -1) * ((index + 1) / 2);
     }
 }
--- test/leetcode/q18_4sum/SolutionTest.java
+++ test/leetcode/q18_4sum/SolutionTest.java
@@ -28,4 +28,42 @@ class SolutionTest {
     private void validateBinarySearchClosest(int[] array, int key, int expected) {
         assertEquals(expected, Solution.binarySearchClosest(array, key));
     }
+
+    @Test
+    void getSwingIndex1() {
+        int[] index2expected = {
+                0, 2,
+                1, 3,
+                2, 1,
+                3, 4,
+                4, 0,
+                5, 5,
+                6, 6,
+                7, 7,
+        };
+        for (int i = 0; i < index2expected.length; i += 2) {
+            validateGetSwingIndex(2, 8, index2expected[i], index2expected[i + 1]);
+        }
+    }
+
+    @Test
+    void getSwingIndex2() {
+        int[] index2expected = {
+                0, 6,
+                1, 7,
+                2, 5,
+                3, 4,
+                4, 3,
+                5, 2,
+                6, 1,
+                7, 0,
+        };
+        for (int i = 0; i < index2expected.length; i += 2) {
+            validateGetSwingIndex(6, 8, index2expected[i], index2expected[i + 1]);
+        }
+    }
+
+    private void validateGetSwingIndex(int closestIndex, int length, int index, int expected) {
+        assertEquals(expected, Solution.getSwingIndex(closestIndex, length, index));
+    }
 }
