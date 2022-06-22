bd9744333d673121524fe0473ed698354f80de43
fanhl <dyguests@qq.com>
2022-06-22 22:11:54 +0800

    find median.

---
+++ src/leetcode/q295_find_median/MedianFinder.java
@@ -0,0 +1,16 @@
+package leetcode.q295_find_median;
+
+class MedianFinder {
+
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+        return 0f;
+    }
+}
---
+++ src/leetcode/q4_meidan/Solution.java
@@ -0,0 +1,7 @@
+package leetcode.q4_meidan;
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0;
+    }
+}
