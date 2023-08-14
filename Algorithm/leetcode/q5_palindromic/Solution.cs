feeb0cce37ab7f28d2eddb1cd2abd3eaccd40000
dyguests <dyguests@qq.com>
2023-08-14 18:16:45 +0800

    hollow

--- Algorithm/leetcode/q5_palindromic/Solution.cs
+++ Algorithm/leetcode/q5_palindromic/Solution.cs
@@ -4,30 +4,11 @@ public class Solution
 {
     public string LongestPalindrome(string str)
     {
-        var result = "";
-        for (var i = 0; i < str.Length; i++)
-        {
-            for (var j = i; j < str.Length; j++)
-            {
-                if (j - i + 1 <= result.Length) continue;
-
-                var matched = true;
-                for (int k = 0; k <= (j - i) / 2; k++)
-                {
-                    if (str[i + k] != str[j - k])
-                    {
-                        matched = false;
-                        break;
-                    }
-                }
-
-                if (matched)
-                {
-                    result = str.Substring(i, j - i + 1);
-                }
-            }
-        }
-
-        return result;
+        var l = str.Length;
+        if (l <= 1) return str;
+        var offsets = Enumerable.Range(0, l - 1).Select(i => str[i + 1] - str[i]).ToArray();
+        var ol = offsets.Length;
+        var hollow = new int[ol*2-1,1];
+        return "";
     }
 }
