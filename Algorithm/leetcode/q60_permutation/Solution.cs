89178b10854287c87211fd6974c32225635ac173
dyguests <dyguests@qq.com>
2023-08-22 17:22:24 +0800

    GetPermutation

--- Algorithm/leetcode/q60_permutation/Solution.cs
+++ Algorithm/leetcode/q60_permutation/Solution.cs
@@ -1,9 +1,46 @@
+using System.Text;
+
 namespace Algorithm.leetcode.q60_permutation;
 
 public class Solution
 {
     public string GetPermutation(int n, int k)
     {
-        return "213";
+        // var c = A(4, 2);
+        // Console.WriteLine($"c:{c}");
+
+        var numbers = Enumerable.Range(1, n).ToList();
+        var result = new StringBuilder();
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            var ani = A(n, i);
+            var quotient = k / ani;
+            k %= ani;
+            result.Append(numbers[quotient]);
+            numbers.RemoveAt(quotient);
+        }
+
+        return result.ToString();
+    }
+
+    private int A(int a, int b)
+    {
+        if (a <= b) throw new InvalidOperationException("a<=b");
+
+        if (b * 2 > a) b = a - b;
+
+        int r = 1;
+        for (int i = 0; i < b; i++)
+        {
+            r *= (a - i);
+        }
+
+        // for (int i = 2; i <= b; i++)
+        // {
+        //     r /= i;
+        // }
+
+        return r;
     }
 }
---
+++ test/leetcode/q60_permutation/SolutionTest.cs
@@ -0,0 +1,22 @@
+using Algorithm.leetcode.q60_permutation;
+
+namespace Test.leetcode.q60_permutation;
+
+public class SolutionTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1() => Check(3, 3, "213");
+
+  [Test]
+    public void Test2() => Check(4, 9, "2314");
+
+    private static void Check(int n, int k, string expected)
+    {
+        Assert.That(new Solution().GetPermutation(n, k), Is.EqualTo(expected));
+    }
+}
